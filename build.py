49a76f8ff6e702273b85daa4a5d81b173ed7b24e
endorphant <endorphant@tilde.town>
2015-01-21 22:25:31 +0000

    seperated out macro file

--- build.py
+++ build.py
@@ -4,16 +4,19 @@ import glob
 import re
 from subprocess import call
 
-path = "/home/jumblesale/twine/"
+path = "/home/jumblesale/projects/tilde.town-twine/"
 basetwee = "%sbase.twee" % path
+macrotwee = "%smacros.twee" % path
 
 out = open(basetwee,'w')
 
 userPattern = r'^\/home\/([^\/]+)\/.*'
 filePattern    = r'.*\/(.*)\.twee$'
+macroPattern    = r'.*\/(.*)macro(.*)\.twee$'
 
 users = {}
 twees = []
+macros = []
 inits = {}
 
 out.write(':: Start\n')
@@ -22,16 +25,24 @@ out.write('<h2>Choose your fate</h2>')
 for twee in glob.glob("/home/*/ttitt/*.twee"):
 	userMatches = re.match(userPattern, twee)
 	fileMatches = re.match(filePattern, twee)
+        macroMatches = re.match(macroPattern, twee)
 	if not userMatches or not fileMatches:
 		print 'no user / no filename found in %s' % twee
 		continue
 	
 	user     = userMatches.group(1)
 	fileName = fileMatches.group(1)
-	
-	print('including %s (%s) from %s' % (twee, fileName, user))
-	users[user] = 1
-	twees.append(twee)
+        if macroMatches:
+            macroName = macroMatches.group(1)
+
+        if macroMatches:
+	    print('including %s (%s) from %s' % (twee, fileName, user))
+	    users[user] = 1
+	    macros.append(twee)
+        else:   
+	    print('including %s (%s) from %s' % (twee, fileName, user))
+	    users[user] = 1
+	    twees.append(twee)
 
 	if fileName == 'globals.twee':
 		inits[user] = 1
@@ -64,7 +75,18 @@ out.write("\n\n")
 
 out.close()
 
-command = "python %stwee/twee %s > %sindex.html" % (path, basetwee, path)
+out = open(macrotwee, 'w')
+
+for twee in macros:
+	with open(twee) as tweeFile:
+		contents = tweeFile.read()
+		out.write("%s\n" % contents)
+
+out.write("\n\n")
+
+out.close()
+
+command = "python %stwee/twee %s %s > %sindex.html" % (path, basetwee, macrotwee, path)
 
 print "running %s" % command
 
